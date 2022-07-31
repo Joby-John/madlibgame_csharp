71c086b0d46ad4e4aeb45ec1f1157de0e42b8a08
Joby John <joby432john@gmail.com>
2022-07-31 17:31:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace madlibgame_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your favourite color : ");
+            string color = Console.ReadLine();
+            Console.Write("Enter your favourite object : ");
+            string thing =  Console.ReadLine();
+            Console.Write("Enter your lovers name: ");
+            string lover = Console.ReadLine();
+            lover = char.ToUpper(lover[0]) + lover.Substring(1);//converting first letter to capital as !totitle function 
+            Console.Write("\nThe poem be : \n");
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(thing + " are blue");
+            Console.WriteLine("I Love " + lover);
+        }
+    }
+}
